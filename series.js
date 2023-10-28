57349ab29c6624b2572d8e7401c446abf8072bdc
unknown <rm553746@fiap.com>
2023-10-27 23:33:21 -0300

    adicionando arquivo series

---
+++ series.js
@@ -0,0 +1,9 @@
+// 1 - Criar um arquivo JavaScript, com o nome series.js
+//  2 - No arquivo filmes.js, armazene em uma constante "my3FavoriteTVShows" suas 3 séries favoritas;
+//   Exemplo: const my3FavoriteTVShows = ["Cidade de Deus", "Sintonia", "Última parada"];
+//  3 - Exiba no console um boolean indicando se o array de séries que você criou  
+//     possui 3 itens
+//   Exemplo: Fazer uma verificação, utilizando operador ===
+
+const my3FavoriteTVShows = ["Brigerton", "Lupin", "Outher Banks"];
+console.log(my3FavoriteTVShows.length === 3); 
